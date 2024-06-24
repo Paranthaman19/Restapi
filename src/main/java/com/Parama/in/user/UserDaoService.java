8faaa74b3aaf07f5cf4af3e206a0e558a572253f
Paranthaman19 <paranthamanat19@gmail.com>
2024-06-24 14:55:22 +0530

    POST will be implemented (post refers to adding new user)

--- src/main/java/com/Parama/in/user/UserDaoService.java
+++ src/main/java/com/Parama/in/user/UserDaoService.java
@@ -10,18 +10,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDaoService {
     private static List<User> users=new ArrayList<>();
+    private static  int usercount=0;
     static {
-    	users.add(new User(1,"chandru",LocalDate.now().minusYears(20 )));
-    	users.add(new User(1,"tharun",LocalDate.now().minusYears(21 )));
-    	users.add(new User(1,"viki",LocalDate.now().minusYears(30 )));	
+    	users.add(new User(++usercount,"chandru",LocalDate.now().minusYears(20 )));
+    	users.add(new User(++usercount,"tharun",LocalDate.now().minusYears(21 )));
+    	users.add(new User(++usercount,"viki",LocalDate.now().minusYears(30 )));	
     }
 	
      public List<User> findAll(){
     	 return users;
     	 
      }
+     public User save(User user) {
+    	 user.setId(++usercount);
+    	 users.add(user);
+    	 return user;
+     }
      public User findOne(int id){
     	 Predicate<? super User> predicate =user -> user.getId().equals(id);
     	 return users.stream().filter(predicate).findFirst().get();
      }
+     
 }
--- src/main/java/com/Parama/in/user/UserResources.java
+++ src/main/java/com/Parama/in/user/UserResources.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +23,9 @@ public class UserResources {
 	   public User retrieveUser(@PathVariable int id){
 		     return service.findOne(id);
 		     }
-       
+       @PostMapping("/user")
+       public void createUsers(@RequestBody User user ) {
+    	   service.save(user);
+       }
        
 }
